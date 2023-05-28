7fab2ef61e0b4155a8621b80d817c65e03ee0aef
ionizy <ionizy@gmail.com>
2023-05-28 20:08:04 +0300

    new lesson

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача1
+
+Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if ((x>0)&&(y>0))
+{
+    Console.WriteLine("1");
+}
+
+else if ((x<0)&&(y>0))
+{
+    Console.WriteLine("2");
+}
+
+else if ((x<0)&&(y<0))
+{
+    Console.WriteLine("3");
+}
+
+else if ((x>0)&&(y<0))
+{
+    Console.WriteLine("4");
+}
