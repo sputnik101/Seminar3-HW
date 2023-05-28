c75952328cf238d5bdadb0a72767edbe0196bbc6
ionizy <ionizy@gmail.com>
2023-05-28 21:49:59 +0300

    new lesson comlete

--- Program.cs
+++ Program.cs
@@ -1,27 +1,120 @@
 ﻿//Задача1
 
-Console.WriteLine("Введите число");
-int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int x = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число");
-int y = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int y = Convert.ToInt32(Console.ReadLine());
 
-if ((x>0)&&(y>0))
-{
-    Console.WriteLine("1");
-}
+// if ((x>0)&&(y>0))
+// {
+//     Console.WriteLine("1");
+// }
 
-else if ((x<0)&&(y>0))
-{
-    Console.WriteLine("2");
-}
+// else if ((x<0)&&(y>0))
+// {
+//     Console.WriteLine("2");
+// }
 
-else if ((x<0)&&(y<0))
-{
-    Console.WriteLine("3");
-}
+// else if ((x<0)&&(y<0))
+// {
+//     Console.WriteLine("3");
+// }
 
-else if ((x>0)&&(y<0))
-{
-    Console.WriteLine("4");
-}
+// else if ((x>0)&&(y<0))
+// {
+//     Console.WriteLine("4");
+// }
+
+//Задача2
+
+//SWITCH!!!
+// Console.WriteLine("Введите число от 1 до 4");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// switch (num)
+// {
+//     case 1:
+//     {
+//         Console.WriteLine("x>0, y>0");
+//         break;
+//     }
+
+//       case 2:
+//     {
+//         Console.WriteLine("x<0, y>0");
+//         break;
+//     }
+
+//       case 3:
+//     {
+//         Console.WriteLine("x<0, y<0");
+//         break;
+//     }
+
+//       case 4:
+//     {
+//         Console.WriteLine("x>0, y<0");
+//         break;
+//     }
+//     default:
+//     {
+//         Console.WriteLine("Некорректное число");
+//         break;
+//     }
+// }
+
+//IF!!!
+// if (num==1)
+// {
+//     Console.WriteLine("x>0, y>0");
+// }
+
+// else if (num==2)
+// {
+//     Console.WriteLine("x<0, y>0");
+// }
+
+// else if (num==3)
+// {
+//     Console.WriteLine("x<0, y<0");
+// }
+
+// else if (num==4)
+// {
+//     Console.WriteLine("x>0, y<0");
+// }
+// else
+// {
+//     Console.WriteLine("Некорректное число");
+// }
+
+//ЗАДАЧА3
+
+// Console.WriteLine("Введите число");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+
+// double d = Math.Sqrt(Math.Pow ((x2-x1),2)+ Math.Pow ((y2-y1),2));
+// Console.WriteLine(d);
+
+//ЗАДАЧА4
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int count=1;
+
+// while (num>=count)
+// {
+//     Console.WriteLine(Math.Pow (count,2));
+//     count=count+1;
+// }
