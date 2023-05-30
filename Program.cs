2e4d912b23931cf8553e5a6e6b49e1e34e3062cb
ionizy <ionizy@gmail.com>
2023-05-30 05:44:03 +0300

    complete hw

--- Program.cs
+++ Program.cs
@@ -118,3 +118,72 @@
 //     Console.WriteLine(Math.Pow (count,2));
 //     count=count+1;
 // }
+//____________________ДОМАШНЕЕ ЗАДАНИЕ__________________________
+
+//Задача1
+
+// Console.WriteLine("Введите пятизначное число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if ((num<10000)||(num>99999))
+// {
+//     Console.WriteLine($"Число {num} не пятизначное.");
+    
+// }
+// else
+// {
+//     int a=num%10;
+//     int b=(num%100)/10;
+//     int c=(num/1000)%10;
+//     int d=num/10000;
+
+//     if (a==d&&b==c)
+//     {
+//         Console.WriteLine($"Число {num} - полиндром.");
+//     }
+    
+//     else
+//     {
+//         Console.WriteLine($"Число {num} - не полиндром.");
+//     }
+// }
+
+//ЗАДАЧА2
+
+// //Первая точка
+// Console.WriteLine("Введите координату x первой точки.");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату y первой точки.");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату z первой точки.");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("~~~~~~~~~~~~~~~~~~");
+// //Вторая точка
+
+// Console.WriteLine("Введите координату x второй точки.");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату y второй точки.");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату z второй точки.");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+
+// double d = Math.Sqrt(Math.Pow ((x2-x1),2)+ Math.Pow ((y2-y1),2)+Math.Pow ((z2-z1),2));
+// Console.WriteLine($"Расстояние между первой и второй точкой равняется {d}");
+
+//ЗАДАЧА3
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int count=1;
+
+// while (num>=count)
+// {
+//     Console.WriteLine(Math.Pow (count,3));
+//     count=count+1;
+// }
